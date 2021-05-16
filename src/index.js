6a54ca53c91a2d940ff099cede69a440bae7b0b9
André Kunde <andre.kunde@zup.com.br>
2021-05-16 16:30:59 -0300

    Creating account

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+const customers = [];
 
 app.use(express.json());
 
@@ -20,30 +23,31 @@ app.use(express.json());
  * Body Params  => Os objetos inserção/alteração (JSON)
  */
 
-app.get("/courses", (request, response) => {
-    const query = request.query;
-    console.log(query);
-    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
-});
+/**
+ * cpf  - string
+ * name - string
+ * id   - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
 
-app.post("/courses", (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
 
-app.put("/courses/:id", (request, response) => {
-    const { id } = request.params;
-    console.log(id);
-    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
 
-app.patch("/courses/:id", (request, response) => {
-    return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
 
-app.delete("/courses/:id", (request, response) => {
-    return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+    return response.status(201).send();
 });
 
 app.listen(3333);
